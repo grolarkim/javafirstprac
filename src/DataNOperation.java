42a235b855b8d4856cc7444f6d717842a1a2fa6c
user <user@userui-MacBookAir.local>
2022-04-18 12:26:30 +0900

    data1

---
+++ src/DataNOperation.java
@@ -0,0 +1,18 @@
+public class DataNOperation {
+    public static void main(String[] args) {
+        System.out.println(6); //Number
+        System.out.println("six"); //String
+        System.out.println("6"); //String
+        System.out.println(6+6); //12
+        System.out.println("6" + "6"); //66
+        System.out.println("1111".length());//4
+
+        System.out.println(6+2);
+        System.out.println(6-2);
+        System.out.println(6*2);
+        System.out.println(6/2);
+        System.out.println(Math.PI);
+        System.out.println(Math.floor(Math.PI));
+        System.out.println(Math.ceil(Math.PI));
+    }
+}

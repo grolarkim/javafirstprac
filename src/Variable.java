bc7566a098fbc24edf3a529da37694e156de5683
user <user@userui-MacBookAir.local>
2022-04-20 17:09:40 +0900

    variable

---
+++ src/Variable.java
@@ -0,0 +1,32 @@
+public class Variable {
+    public static void main(String[] args) {
+        // int a = 1; //Number -> integer
+        // System.out.println(a);
+        // double b = 1.1;    //real number -> double
+        // System.out.println(b);
+        // String c = "Hello World";
+        // System.out.println(c);
+
+        String name = "aaa";
+        System.out.println("Hello, "+name+" ... "+name+" ... egoing ... bye");
+        
+        double VAT = 10.0;
+        System.out.println(VAT);
+
+        double a = 1.1;
+        double b = 1;
+        double b2 = (double) 1;
+        
+        System.out.println(b);
+        
+        // int c = 1.1;
+        double d = 1.1;
+        int e = (int) 1.1;
+        System.out.println(e);
+        
+        // 1 to String 
+        String f = Integer.toString(1);
+        System.out.println(f.getClass());
+
+    }
+}

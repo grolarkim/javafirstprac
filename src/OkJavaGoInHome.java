f907ded6319316a2fdbcebd90a4fb0a408e39569
user <user@userui-MacBookAir.local>
2022-04-28 16:19:23 +0900

    20220428-java1.11.1

--- src/OkJavaGoInHome.java
+++ src/OkJavaGoInHome.java
@@ -1,23 +1,34 @@
+import javax.swing.JOptionPane;
+
+import org.opentutorials.iot.DimmingLights;
 import org.opentutorials.iot.Elevator;
 import org.opentutorials.iot.Lighting;
 import org.opentutorials.iot.Security;
 
 public class OkJavaGoInHome {
     public static void main(String[] args) {
+
+        String id = JOptionPane.showInputDialog("Enter an ID");
+        String bright = JOptionPane.showInputDialog("Enter Bright");
+
         // Elevator call
-        Elevator myElevator = new Elevator("JAVA APT 507");
+        Elevator myElevator = new Elevator(id);
         myElevator.callForUp(1);
 
         // Security off
-        Security mySecurity = new Security("JAVA APT 507");
+        Security mySecurity = new Security(id);
         mySecurity.off();
 
         // Light on
-        Lighting hallLamp = new Lighting("JAVA APT 507 / Hall Lamp");
+        Lighting hallLamp = new Lighting(id +" / Hall Lamp");
         hallLamp.on();
         
-        Lighting floorLamp = new Lighting("JAVA APT 507 / Floor Lamp");
+        Lighting floorLamp = new Lighting(id + " / Floor Lamp");
         floorLamp.on();
+
+        DimmingLights moodLamp = new DimmingLights(id + " moodLamp");
+        moodLamp.setBright(Double.parseDouble(bright));
+        moodLamp.on();
 
     }
 }
